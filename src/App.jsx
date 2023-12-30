164d515c438c8b13e020ba1f255b497d71fb7ed5
Xavier-Hsiao <tim88776@gmail.com>
2023-12-30 22:06:07 +0800

    Init: initialize project

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import FriendList from "./component/FriendList/FriendList";
+import styles from "./App.module.scss";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <div className={styles.sidebar}>
+        <FriendList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
